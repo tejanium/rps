54543774a9f8bf4d95e755dc87e2aa6462f771b1
Teja Sophista <tejanium@yahoo.com>
2022-08-26 15:20:51 +0100

    Tests

---
+++ test/rps_web/features/random_game_test.exs
@@ -0,0 +1,84 @@
+defmodule RpsWeb.Features.RandomGameTest do
+  use ExUnit.Case, async: true
+  use Wallaby.Feature
+
+  import Wallaby.Query, only: [button: 1]
+  @page "/"
+
+  def assert_has_all_buttons(session) do
+    session
+    |> assert_has(button("rock"))
+    |> assert_has(button("paper"))
+    |> assert_has(button("scissor"))
+  end
+
+  @sessions 2
+  feature "Users play with each other", %{sessions: [user_1, user_2]} do
+    on_exit(fn ->
+      Wallaby.end_session(user_1)
+      Wallaby.end_session(user_2)
+    end)
+
+    # Preparing the game
+    user_1
+    |> visit(@page)
+    |> assert_text("Searching for opponent... Please wait!")
+
+    user_2
+    |> visit(@page)
+    |> assert_has_all_buttons()
+    |> assert_text("playing against")
+
+    user_1
+    |> assert_has_all_buttons()
+    |> assert_text("playing against")
+
+    # Start playing
+    user_1
+    |> click(button("rock"))
+    |> assert_text("Waiting for opponent to pick their move...")
+
+    user_2
+    |> assert_text("Opponent has picked their move.")
+
+    user_2
+    |> click(button("scissor"))
+
+    assert_text(user_1, "You win!")
+    assert_text(user_2, "You lose!")
+
+    # New game
+    user_1
+    |> assert_has_all_buttons()
+
+    user_2
+    |> assert_has_all_buttons()
+
+    assert_text(user_1, "Win: 1, Lose: 0")
+    assert_text(user_2, "Win: 0, Lose: 1")
+  end
+
+  @sessions 2
+  feature "One user disconnected", %{sessions: [user_1, user_2]} do
+    on_exit(fn ->
+      Wallaby.end_session(user_1)
+      Wallaby.end_session(user_2)
+    end)
+
+    # Preparing the game
+    user_1
+    |> visit(@page)
+
+    user_2
+    |> visit(@page)
+    |> assert_has_all_buttons()
+
+    # One user leaving the page
+    user_1
+    |> assert_has_all_buttons()
+    |> visit("/")
+
+    user_2
+    |> assert_text("The opponent has been disconnected, please refresh to start new game.")
+  end
+end
