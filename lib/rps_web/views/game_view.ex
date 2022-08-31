e6eaba97e52f29d032d3c753e71a0bed3d11c35c
Teja Sophista <tejanium@yahoo.com>
2022-08-31 09:57:31 +0100

    Stats

--- lib/rps_web/views/game_view.ex
+++ lib/rps_web/views/game_view.ex
@@ -1,3 +1,17 @@
 defmodule RpsWeb.GameView do
   use RpsWeb, :view
+
+  def humanize_standing(id, turns) do
+    turns = Map.values(turns)
+
+    win = Enum.count turns, fn turn ->
+      turn[:winner] && turn.winner == id
+    end
+
+    lose = Enum.count turns, fn turn ->
+      turn[:winner] && turn.winner != id
+    end
+
+    "Win: #{win}, Lose: #{lose}"
+  end
 end
